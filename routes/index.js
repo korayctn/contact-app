e3583a548928ca80312b3c41359e50cde5154344
koraycetin <korayctnx06@gmail.com>
2022-06-29 14:18:48 +0300

    Some adjustments

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+/* GET home page. */
+
+router.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+module.exports = router;
